dbcefa4cf108ecee06885f0dd352977ddc824ed5
Yuttapoom Haphanom <56127401+AUUNNG@users.noreply.github.com>
2024-12-15 16:21:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ class TaxController:
         taxable_income -= 60000
         taxable_income -= self._deductions
         if taxable_income >= 5000001:
-        return ((taxable_income-5000000)*.35) + 0 + 7500 + 20000 + 37500 + 50000 + 250000 + 900000
-    elif taxable_income >= 2000001:
-        return ((taxable_income-2000000)*.3) + 0 + 7500 + 20000 + 37500 + 50000 + 250000
-    elif taxable_income >= 1000001:
-        return ((taxable_income-1000000)*.25) + 0 + 7500 + 20000 + 37500 + 50000
-    elif taxable_income >= 750001:
-        return ((taxable_income-750000)*.2) + 0 + 7500 + 20000 + 37500
-    elif taxable_income >= 500001:
-        return ((taxable_income-500000)*.15) + 0 + 7500 + 20000
-    elif taxable_income >= 300001:
-        return ((taxable_income-300000)*.1) + 0 + 7500
-    elif taxable_income >= 1500001:
-        return ((taxable_income-150000)*.05) + 0
-    elif taxable_income < 1500001:
-        return 0
+            return ((taxable_income-5000000)*.35) + 0 + 7500 + 20000 + 37500 + 50000 + 250000 + 900000
+        elif taxable_income >= 2000001:
+            return ((taxable_income-2000000)*.3) + 0 + 7500 + 20000 + 37500 + 50000 + 250000
+        elif taxable_income >= 1000001:
+            return ((taxable_income-1000000)*.25) + 0 + 7500 + 20000 + 37500 + 50000
+        elif taxable_income >= 750001:
+            return ((taxable_income-750000)*.2) + 0 + 7500 + 20000 + 37500
+        elif taxable_income >= 500001:
+            return ((taxable_income-500000)*.15) + 0 + 7500 + 20000
+        elif taxable_income >= 300001:
+            return ((taxable_income-300000)*.1) + 0 + 7500
+        elif taxable_income >= 1500001:
+            return ((taxable_income-150000)*.05) + 0
+        elif taxable_income < 1500001:
+            return 0
 
     def __str__(self):
         return f"Name: {self._name}, Income: {self._income}, Deductions: {self._deductions}, Tax Due: {self.calculate_tax()}"
